d721c2b16c585d0339f2f02b17d3e4e3044291dc
rndomhack <rndom.hack@gmail.com>
2015-11-11 17:56:33 +0900

    First commit

---
+++ test/test.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const fs = require("fs");
+const TsStream = require("../lib/stream");
+
+const readStream = fs.createReadStream(process.argv[2]);
+const tsStream = new TsStream({});
+
+var fileSize = fs.statSync(process.argv[2]).size;
+var loadSize = 0;
+var count = 0;
+
+console.time("load");
+
+readStream.pipe(tsStream);
+
+tsStream.on("data", data => {
+    loadSize += data.length;
+    if (++count % 100000 === 0) console.log(count, loadSize / fileSize * 100);
+});
+
+/*
+tsStream.on("packet", packet => {
+    loadSize += packet.raw.length;
+    if (packet.PID === 0x140 && packet.adaptation_field_control >> 1 === 1) {
+        //console.log(packet);
+    }
+    if (++count % 100000 === 0) {
+        console.log(count, loadSize / fileSize * 100);
+    }
+});
+*/
+
+tsStream.on("end", () => {
+    console.log(count, loadSize / fileSize * 100);
+    console.timeEnd("load");
+});
