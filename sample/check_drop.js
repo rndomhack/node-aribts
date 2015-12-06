cef823867ad9768b098259887654823b9df772e6
rndomhack <rndom.hack@gmail.com>
2015-12-06 18:00:20 +0900

    Add sample check_drop.js

---
+++ sample/check_drop.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const fs = require("fs");
+const aribts = require("../index");
+const TsStream = aribts.TsStream;
+const TsUtil = aribts.TsUtil;
+
+const readStream = fs.createReadStream(process.argv[2]);
+const tsStream = new TsStream();
+
+const tsUtil = new TsUtil();
+
+readStream.pipe(tsStream);
+
+tsStream.on("data", () => {});
+
+tsStream.on("drop", pid => {
+    var time = tsUtil.isTime() ? `${("0" + tsUtil.getTime().getHours()).slice(-2)}:${("0" + tsUtil.getTime().getMinutes()).slice(-2)}:${("0" + tsUtil.getTime().getSeconds()).slice(-2)}` : "unknown";
+    console.log(`pid: 0x${pid.toString(16)}, time: ${tsUtil.isTime() ? time : "unknown"}`);
+});
+
+tsStream.on("info", data => {
+    console.log("");
+    console.log("info:");
+    Object.keys(data).forEach(key => {
+        console.log(`0x${("000" + parseInt(key, 10).toString(16)).slice(-4)}: packet: ${data[key].packet}, drop: ${data[key].drop}, scrambling: ${data[key].scrambling},`);
+    });
+});
+
+tsStream.on("tdt", (pid, data) => {
+    tsUtil.addTdt(pid, data);
+});
+
+tsStream.on("tot", (pid, data) => {
+    tsUtil.addTot(pid, data);
+});
