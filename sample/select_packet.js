eb859c61d68647e6b4d74c50b7353705f48bcb06
rndomhack <rndom.hack@gmail.com>
2016-03-22 09:15:43 +0900

    Fix add PCR PID

--- sample/select_packet.js
+++ sample/select_packet.js
@@ -1,11 +1,29 @@
 "use strict";
 
 const fs = require("fs");
+const stream = require("stream");
 const aribts = require("../index");
 const TsStream = aribts.TsStream;
 
+var size = fs.statSync(process.argv[2]).size;
+var bytesRead = 0;
+
 const readStream = fs.createReadStream(process.argv[2]);
 const writeStream = fs.createWriteStream(process.argv[3]);
+const transformStream = new stream.Transform({
+    transform: function (chunk, encoding, done) {
+        bytesRead += chunk.length;
+
+        console.log("\u001b[2A");
+        console.log(`Transform - ${bytesRead} of ${size} [${Math.floor(bytesRead / size * 100)}%]`);
+
+        this.push(chunk);
+        done();
+    },
+    flush: function (done) {
+        done();
+    }
+});
 const tsStream = new TsStream({
     transform: true,
     transPmtIds: [0],
@@ -13,7 +31,8 @@ const tsStream = new TsStream({
     transPids: new Array(0x32).fill(0x00).map((value, index) => index)
 });
 
-readStream.pipe(tsStream);
+readStream.pipe(transformStream);
+transformStream.pipe(tsStream);
 tsStream.pipe(writeStream);
 
 tsStream.on("info", data => {
