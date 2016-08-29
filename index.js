6d48b9625edbf9394f4005f3caf8a3501796afa6
rndomhack <rndom.hack@gmail.com>
2016-08-29 20:58:01 +0900

    Fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = {
     TsCrc32: require("./lib/crc32"),
     TsChar: require("./lib/char"),
     TsDate: require("./lib/date"),
+    TsLogo: require("./lib/logo"),
     TsDescriptors: require("./lib/descriptors"),
     TsModuleDescriptors: require("./lib/module_descriptors"),
-    TsLogo: require("./lib/logo"),
     TsUtil: require("./lib/util"),
     tsTable: require("./lib/table"),
     tsDataModule: require("./lib/data_module")
